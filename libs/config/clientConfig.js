bc9d489ec5456b76985a0f1c6c3f2a0be9b78d29
DaoQuangThiet <67154017+DaoQuangThiet@users.noreply.github.com>
2022-05-12 10:30:56 +0700

    Update clientConfig.js

--- libs/config/clientConfig.js
+++ libs/config/clientConfig.js
@@ -1,6 +1,6 @@
 const clientConfig = {
     siteUrl: 'http://localhost:3000',
-    graphQlUrl: 'https://localhost:3000/shop-api?vendure-token=85cdpp6kwjvh5dou7cit'
+    graphQlUrl: 'https://localhost:3000/shop-api'
 };
 
 module.exports = clientConfig;
