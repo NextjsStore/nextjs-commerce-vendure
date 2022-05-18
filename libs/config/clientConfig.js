b4139482a80ecf1a8dc59759ef1de1f5020ae4f6
DaoQuangThiet <67154017+DaoQuangThiet@users.noreply.github.com>
2022-05-18 12:34:09 +0700

    Update clientConfig.js

--- libs/config/clientConfig.js
+++ libs/config/clientConfig.js
@@ -1,6 +1,6 @@
 const clientConfig = {
     siteUrl: 'http://localhost:3000',
-    graphQlUrl: 'http://localhost:3000/shop-api?vendure-token=xn5i72fr18t00v9ghbm'
+    graphQlUrl: 'https://commerce.cmsmart.net/shop-api?vendure-token=eg7bykxkhyd5lszzdnf'
 };
 
 module.exports = clientConfig;
