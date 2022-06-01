01f70e059796f7b6c0495572c0c501698be3c34a
DaoQuangThiet <67154017+DaoQuangThiet@users.noreply.github.com>
2022-06-01 10:48:02 +0700

    clear

--- libs/config/clientConfig.js
+++
@@ -1,6 +0,0 @@
-const clientConfig = {
-    siteUrl: 'http://localhost:3000',
-    graphQlUrl: 'https://commerce.cmsmart.net/shop-api?vendure-token=eg7bykxkhyd5lszzdnf'
-};
-
-module.exports = clientConfig;
