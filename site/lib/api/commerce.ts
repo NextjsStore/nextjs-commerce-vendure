2d98c73c306fc943e3a82456953109bee3df0f6c
DaoQuangThiet <67154017+DaoQuangThiet@users.noreply.github.com>
2022-06-18 16:02:03 +0700

    add collections

--- site/lib/api/commerce.ts
+++ site/lib/api/commerce.ts
@@ -1,10 +1,3 @@
 import { getCommerceApi } from '@framework/api'
 
 export default getCommerceApi()
-
-export const testApi = () => {
-  const url = process.env.NEXT_PUBLIC_VENDURE_SHOP_API_URL
-  const token = process.env.VENDURE_TOKEN
-
-  return fetch(`${url}?vendure-token=${token}`)
-}
